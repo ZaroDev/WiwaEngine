0fcd486a9f0dddac90f294380ac83c998a3ae99c
Victor <victorfalcon796@gmail.com>
2022-11-12 22:34:27 +0100

    Input marshall

---
+++ Wiwa-ScriptCore/Source/Wiwa/Input.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiwa
+{
+    public class Input
+    {
+        public static bool IsKeyDown(KeyCode keyCode)
+        {
+            return InternalCalls.IsKeyDownIntr(keyCode);
+        }
+        public static bool IsMouseKeyDown(int button)
+        {
+            return InternalCalls.IsMouseButtonPressedIntr(button);
+        }
+    }
+}
