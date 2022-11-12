6d94884a9ac7f329c1e71b780744055707ae58cc
Victor <victorfalcon796@gmail.com>
2022-11-12 17:35:25 +0100

    Script Engine refactor & ScriptGlue
    - Added internal callbacks
    - Added scriptclass

--- Wiwa/src/Wiwa/scripting/ScriptEngine.h
+++ Wiwa/src/Wiwa/scripting/ScriptEngine.h
@@ -1,13 +1,46 @@
 #pragma once
 
+#include <filesystem>
+#include <string>
+extern "C" {
+	typedef struct _MonoClass MonoClass;
+	typedef struct _MonoObject MonoObject;
+	typedef struct _MonoMethod MonoMethod;
+}
+
+
 namespace Wiwa {
+	
+
 	class ScriptEngine
 	{
 	public:
 		static void Init();
 		static void ShutDown();
+
+		static void LoadAssembly(const std::filesystem::path& filepath);
 	private:
 		static void InitMono();
 		static void ShutDownMono();
+
+		static MonoObject* InstantiateClass(MonoClass* monoClass);
+
+		friend class ScriptClass;
+	};
+
+	class ScriptClass
+	{
+	public:
+		ScriptClass() = default;
+		ScriptClass(const std::string& classNamespace, const std::string& className);
+		
+		MonoObject* Instantiate();
+		MonoMethod* GetMethod(const std::string& name, int parameterCount);
+		MonoObject* InvokeMethod(MonoObject* instance, MonoMethod* method, void** params = nullptr);
+	private:
+		std::string m_ClassNamespace;
+		std::string m_ClassName;
+
+		MonoClass* m_MonoClass = nullptr;
 	};
 }
---
+++ Wiwa/src/Wiwa/scripting/ScriptGlue.cpp
@@ -0,0 +1,32 @@
+#include <wipch.h>
+#include "ScriptGlue.h"
+
+#include <mono/metadata/object.h>
+#include <glm.hpp>
+
+namespace Wiwa {
+
+#define WI_ADD_INTERNAL_CALL(Name) mono_add_internal_call("Wiwa.InternalCalls::" #Name, Name)
+	static void CppFunc()
+	{
+		std::cout << "This is written in C++" << std::endl;
+	}
+	static void NativeLog(MonoString* string, int parameter)
+	{
+		char* str = mono_string_to_utf8(string);
+
+		WI_CORE_TRACE("{0} {1}", str, parameter);
+		mono_free(str);
+	}
+	static void NativeLogVector(glm::vec3* parameter, glm::vec3* outParam)
+	{
+		WI_CORE_TRACE("Value of X:{0}, Y:{1}, Z{2}", parameter->x, parameter->y, parameter->z);
+		*outParam = glm::normalize(*parameter);
+	}
+	void ScriptGlue::RegisterFunctions()
+	{
+		WI_ADD_INTERNAL_CALL(CppFunc);
+		WI_ADD_INTERNAL_CALL(NativeLog);
+		WI_ADD_INTERNAL_CALL(NativeLogVector);
+	}
+}
