9708e00d0f69ff7bb4a43540b2dcb6dee00eb28a
Victor <victorfalcon796@gmail.com>
2022-11-13 20:20:24 +0100

    AddComponent cs

--- Wiwa-ScriptCore/Source/Wiwa/Behaviour.cs
+++ Wiwa-ScriptCore/Source/Wiwa/Behaviour.cs
@@ -10,5 +10,17 @@
             T component = Utils.Deserialize<T>(bytes);
             return component;
         }
+        public T AddComponent<T>(EntityId id) where T : unmanaged
+        {
+            byte[] bytes = InternalCalls.AddComponent(id, typeof(T), null);
+            T component = Utils.Deserialize<T>(bytes);
+            return component;
+        }
+        public T AddComponent<T>(EntityId id, T component) where T : unmanaged
+        {
+            byte[] bytes = Utils.Serialize(component);
+            InternalCalls.AddComponent(id, typeof(T), bytes);
+            return component;
+        }
     }
 }
